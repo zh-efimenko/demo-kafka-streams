49b0220641714afdfdd50f15ca4a6057d3580346
yauheniyefimenka <yauheni.yefimenka@dataart.com>
2023-09-26 21:27:32 +0400

    added initialization of topics

---
+++ etc/docker/kafka/init-kafka-topics.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+echo "Waiting for Kafka to start..."
+while ! nc -z kafka 29092; do
+  sleep 1
+done
+echo "Kafka is running, creating topics...!"
+
+topics=(
+  lesson1_source
+  lesson1_target
+  lesson1_target_ksql
+
+  lesson2_target
+
+  lesson3_source
+  lesson3_target
+
+  lesson4_split_source
+  lesson4_left_source
+  lesson4_right_source
+  lesson4_target
+
+  lesson5_source
+  lesson5_target
+
+  lesson6_source
+  lesson6_target
+)
+
+for topic in "${topics[@]}"; do
+  kafka-topics --bootstrap-server kafka:29092 --create --if-not-exists --topic "$topic"
+done
+
+kafka-topics --bootstrap-server kafka:29092 --create --partitions 3 --if-not-exists --topic lesson2_source
+
+echo "All topics were successfully created!"
